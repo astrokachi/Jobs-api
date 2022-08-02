574eb53de8f1ef6833e8f299a61db2ab1e28826e
astrokachi <astrokachi@gmail.com>
2022-08-02 03:51:26 +0100

    first commit

---
+++ middleware/authentication.js
@@ -0,0 +1,25 @@
+const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const { UnauthenticatedError } = require("../errors");
+
+const auth = (req, res, next) => {
+	//check header
+	const authHeader = req.headers.authorization;
+	if (!authHeader || !authHeader.startsWith("Bearer"))
+		throw new UnauthenticatedError("Authentication Invalid");
+
+	const token = authHeader.split(" ")[1];
+
+	try {
+		const payload = jwt.verify(token, process.env.JWT_SECRET);
+		//attach user to the job route
+		// const user = User.findById(payload.userId).select("-password");
+    // req.user = user
+		req.user = { userId: payload.userId, name: payload.name };
+		next();
+	} catch (error) {
+		throw new UnauthenticatedError("Authentication Invalid");
+	}
+};
+
+module.exports = auth;
---
+++ models/User.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const UserSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, "Please enter a name"],
+		minlength: 4,
+		maxlength: 50,
+	},
+
+	email: {
+		type: String,
+		required: [true, "Please enter a email"],
+		minlength: 4,
+		maxlength: 50,
+		match: [
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+			"please provide a valid email",
+		],
+		unique: true,
+	},
+
+	password: {
+		type: String,
+		required: [true, "Please enter a password"],
+		minlength: 6,
+	},
+});
+
+UserSchema.pre("save", async function (next) {
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+	next();
+});
+
+UserSchema.methods.createJWT = function () {
+	return jwt.sign(
+		{ userId: this._id, name: this.name },
+		process.env.JWT_SECRET,
+		{
+			expiresIn: process.env.JWT_EXP,
+		}
+	);
+};
+
+UserSchema.methods.comparePasswords = async function (candidatePassword) {
+	const isMatch = await bcrypt.compare(candidatePassword, this.password);
+	return isMatch;
+};
+
+module.exports = mongoose.model("User", UserSchema);
