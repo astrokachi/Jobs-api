45f3bea4db969191c5d782ca9613cf8dfa39164a
astrokachi <astrokachi@gmail.com>
2022-08-02 04:03:05 +0100

    add procfile and default route

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+
+app.get('/', (req, res) => {
+	res.send('Api up and running')
+})
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
